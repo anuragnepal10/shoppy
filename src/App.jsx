e98946ebf1f730da6b86ce922b9224dc63735a4e
anuragnepal10 <anuragnepal02@gmail.com>
2023-04-15 09:36:23 +0545

    implemented router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,45 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Layout from './components/Layout/Layout'
+import HomePage from './pages/HomePage/HomePage'
+import ProductPage from './pages/ProductPage/ProductPage'
+import CartPage from './pages/CartPage/CartPage'
+import AboutPage from './pages/AboutPage/AboutPage'
+import ErrorPage from './pages/ErrorPage/ErrorPage'
+
 function App() {
-	return <div className="App">hi</div>
+	const router = createBrowserRouter([
+		{
+			path: '/',
+			element: <Layout />,
+			children: [
+				{
+					path: '/',
+					element: <HomePage />,
+				},
+				{
+					path: '/product/:id',
+					element: <ProductPage />,
+				},
+				{
+					path: '/cart',
+					element: <CartPage />,
+				},
+				{
+					path: '/about',
+					element: <AboutPage />,
+				},
+				{
+					path: '*',
+					element: <ErrorPage />,
+				},
+			],
+		},
+	])
+	return (
+		<div className="App">
+			<RouterProvider router={router}></RouterProvider>
+		</div>
+	)
 }
 
 export default App
