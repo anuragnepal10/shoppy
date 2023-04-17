2ff0fb9374037024206b5ed537499dd2ac548720
anuragnepal10 <anuragnepal02@gmail.com>
2023-04-17 09:28:19 +0545

    made cart page responsive

--- src/features/cart/CartCard/CartCard.jsx
+++ src/features/cart/CartCard/CartCard.jsx
@@ -7,7 +7,7 @@ const CartCard = () => {
 		<article className="cart-card">
 			<div className="cart-card-image-container"></div>
 			<div className="cart-card-info-container">
-				<h3>Lorem ipsum dolor sit amet.</h3>
+				<h3 className="cart-card-title">Lorem ipsum dolor sit amet.</h3>
 				<p className="cart-card-price">
 					900 <span>$</span>
 				</p>
